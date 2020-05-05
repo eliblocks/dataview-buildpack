44ca53c2922ac143d27768d6c7d575544e17c091
Eli Block <eliyahu.block@gmail.com>
2020-05-05 09:56:38 -0400

    remove export

--- profile.d/heroku-google-cloud.sh
+++ profile.d/heroku-google-cloud.sh
@@ -1,5 +1,5 @@
-export PATH=/app/vendor/google-cloud-sdk/bin:$
 export PATH=/app/vendor/google-cloud-sdk/bin:$PATH
+
 gcloud auth activate-service-account --key-file /app/google-credentials.json
 gcloud config set project metabase-270301
 gcloud config set compute/zone us-central1-c
